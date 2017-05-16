ecffd96585eb8c75cc932761b1b8f4abde9e7810
cjamadagni <chirag.jamadagni@gmail.com>
2017-05-16 11:11:09 +0530

    Created initial models and admin page

---
+++ ClinicalSupport/myapp/models.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+class clinicUserProfile(models.Model):
+	uid = models.CharField(max_length = 50)
+	smokingHistory = models.BooleanField(default=False)
+	allergies = models.CharField(max_length = 50)
+	cough = models.BooleanField(default=False)
+	fever = models.BooleanField(default=False)
+	hemoptysis = models.BooleanField(default=False)
+	breathlessness = models.BooleanField(default=False)
+	skinrash = models.BooleanField(default=False)
+	tb = models.BooleanField(default=False)
+	cvd = models.BooleanField(default=False)
+	sinusitis = models.BooleanField(default=False)
+	asthma = models.BooleanField(default=False)
+	dm = models.BooleanField(default=False)
+	hiv = models.BooleanField(default=False)
+
+	def __unicode__(self):
+		return self.uid
+
+	
+
